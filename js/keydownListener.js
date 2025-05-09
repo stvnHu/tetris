c2a2bd73d741a00685a6a027ca31d757bb378d5f
stvnHu <stvnhng1@gmail.com>
2025-05-09 04:35:59 +0100

    event listener only works when gameState is "playing"

--- js/keydownListener.js
+++ js/keydownListener.js
@@ -1,22 +1,24 @@
 document.addEventListener("keydown", function (event) {
-  switch (event.key.toLowerCase()) {
-    case "arrowright":
-      moveX(1);
-      break;
-    case "arrowleft":
-      moveX(-1);
-      break;
-    case "arrowdown":
-      moveY();
-      break;
-    case "x":
-      rotate(1);
-      break;
-    case "z":
-      rotate(-1);
-      break;
-    case "c":
-      hold();
-      break;
+  if (gameState === "playing") {
+    switch (event.key.toLowerCase()) {
+      case "arrowright":
+        moveX(1);
+        break;
+      case "arrowleft":
+        moveX(-1);
+        break;
+      case "arrowdown":
+        moveY();
+        break;
+      case "x":
+        rotate(1);
+        break;
+      case "z":
+        rotate(-1);
+        break;
+      case "c":
+        hold();
+        break;
+    }
   }
 });
